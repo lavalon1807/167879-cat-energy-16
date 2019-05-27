3981747966014107dfd6508e1816ee17f5e57503
Egor <Cost_benefit@mail.ru>
2019-05-28 00:02:40 +0300

    Последняя проверка

--- source/js/main.js
+++ source/js/main.js
@@ -1,19 +1,7 @@
       var navMain = document.querySelector('.main-nav');
       var navToggle = document.querySelector('.main-nav__toggle');
-      var gulp = require("gulp");
-      var rename = require("gulp-rename");
-      var svgstore = require("gulp-svgstore");
 
       navMain.classList.remove('main-nav--nojs');
-
-      gulp.task("sprite", function () {
-        return gulp.src("source/img/icon-*.svg")
-          .pipe(svgstore({
-            inLineSvg: true
-          }))
-          .pipe(rename("sprite.svg"))
-          .pipe(gulp.dest("source/img"));
-      })
 
       navToggle.addEventListener('click', function() {
         if (navMain.classList.contains('main-nav--closed')) {
